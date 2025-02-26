7102ce62646a2ea318b38b757584024966f5fe07
BenjaminSierotaKEA <the.crazy.thing.is@gmail.com>
2025-02-26 10:52:23 +0100

    Initial

---
+++ src/main/java/org/example/qodanaclouddemo/QodanaCloudDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.qodanaclouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QodanaCloudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QodanaCloudDemoApplication.class, args);
+    }
+
+}
